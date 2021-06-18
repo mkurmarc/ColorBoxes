f05dabc44fdadbfb0596c904ef130c676ab7cc2f
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-06-18 15:17:18 -0700

    all components complete, need to fix render method of BoxesContainer

--- src/Box.js
+++ src/Box.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import './Box.css';
+import { choice } from "./helpers";
 
 class Box extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: choice(this.props.colors)
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    pickColor() {
+        let newColor;
+        do {
+          newColor = choice(this.props.colors);
+        } while (newColor === this.state.color);
     
-    render() { 
+        this.setState({ color: newColor });
+}
+
+    handleClick() {
+        this.pickColor();
+    }
+
+    render() {
         return (
-            <div className='BoxContainer'></div>
+            <div
+                className='Box'
+                style={{ backgroundColor: this.state.color }}
+                onClick={this.handleClick}
+            />
         );
     }
 }
- 
+
 export default Box;
--- src/BoxesContainer.js
+++ src/BoxesContainer.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
 import Box from './Box';
+import './BoxesContainer.css'
 
 class BoxesContainer extends Component {
     static deafultProps = {
-        colors: [
-            'rgb(242,230,255)', 'rgb(230,204,255)','rgb(217,179,255)'
-        ]
+        numBoxes: 18,
+        allColors: ["red", "blue", "green", "orange", "yellow"]
+        // allColors: [
+        //     "rgb(242,230,255)", 'rgb(230,204,255)', 'rgb(217,179,255)',
+        //     'rgb(204,153,255)', 'rgb(191,128,255)', 'rgb(179,102,255)',
+        //     'rgb(166,77,255)', 'rgb(153,51,255)', 'rgb(140,25,255)',
+        //     'rgb(128,0,255)', 'rgb(115,0,230)', 'rgb(102,0,204)',
+        //     'rgb(80,0,9)', 'rgb(77,0,153)', 'rgb(64,0,128)'
+        // ]
     };
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            color: 'rgb(217,179,255)'
-        }
-    }
-
     
-    render() { 
-        return (
-            <section>
-                <Box />
-                <Box />
-            </section>
-            
-        );
+    render() {
+        const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+          <Box colors={this.props.allColors} />
+        ));
+
+        return <div className='BoxesContainer'>{boxes}</div>;
     }
 }
  
